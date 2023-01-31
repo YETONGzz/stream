d0d12a7ee2d8a306fe8c376bc1b8555c98845489
YeTong <848260181@qq.com>
2023-01-31 14:09:28 +0800

    stream流

--- foreach/src/main/java/com/yetong/Sorted.java
+++ foreach/src/main/java/com/yetong/Sorted.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * 排序
@@ -32,6 +33,12 @@ public class Sorted {
                 .sorted(Comparator.comparing(Person::getSalary).thenComparing(Person::getAge)).map(Person::getName)
                 .collect(Collectors.toList());
         // 先按工资再按年龄自定义排序（降序）
+        List<Person> collect2 = personList.stream().sorted((o1, o2) -> {
+            if (o1.getSalary() == o2.getSalary()) {
+                return o2.getAge() - o1.getAge();
+            }
+            return o2.getSalary() - o1.getSalary();
+        }).collect(Collectors.toList());
         List<String> newList4 = personList.stream().sorted((p1, p2) -> {
             if (p1.getSalary() == p2.getSalary()) {
                 return p2.getAge() - p1.getAge();
@@ -44,6 +51,22 @@ public class Sorted {
         System.out.println("按工资降序排序：" + newList2);
         System.out.println("先按工资再按年龄升序排序：" + newList3);
         System.out.println("先按工资再按年龄自定义降序排序：" + newList4);
+
+        String[] arr1 = { "a", "b", "c", "d" };
+        String[] arr2 = { "d", "e", "f", "g" };
+
+        Stream<String> stream1 = Stream.of(arr1);
+        Stream<String> stream2 = Stream.of(arr2);
+        // concat:合并两个流 distinct：去重
+        List<String> newList1 = Stream.concat(stream1, stream2).distinct().collect(Collectors.toList());
+        // limit：限制从流中获得前n个数据
+        List<Integer> collect3 = Stream.iterate(1, x -> x + 2).limit(10).collect(Collectors.toList());
+        // skip：跳过前n个数据
+        List<Integer> collect4 = Stream.iterate(1, x -> x + 2).skip(1).limit(5).collect(Collectors.toList());
+
+        System.out.println("流合并：" + newList1);
+        System.out.println("limit：" + collect3);
+        System.out.println("skip：" + collect4);
 
     }
 }
